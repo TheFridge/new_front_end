2b886491e06db1d13c00e96c50138c0f8428773d
Billy <navyosu@gmail.com>
2014-02-05 13:38:16 -0700

    Adds format recipes method for list builder

--- app/models/list_builder.rb
+++ app/models/list_builder.rb
@@ -11,6 +11,12 @@ class ListBuilder
     {"user_id" => user.id, "email" => user.email}
   end
 
+  def format_recipes
+    recipes.map do |recipe|
+      {"name" => recipe.name, "source_url" => recipe.source_url, "servings" => recipe.servings, "ingredients" => recipe.ingredients}
+    end
+  end
+
   def prepared_to_send
     {"user" => format_user_information, "recipes" => recipes}
   end
--- spec/models/list_builder_spec.rb
+++ spec/models/list_builder_spec.rb
@@ -4,8 +4,10 @@ describe ListBuilder do
 
   before do
     @user = User.new(email: "email@example.com")
-    @recipe = Recipe.get_recipe
-    @list = ListBuilder.new(@user, @recipe)
+    recipe1 = Recipe.get_recipe
+    recipe2 = Recipe.get_recipe
+    @recipes = [recipe1, recipe2]
+    @list = ListBuilder.new(@user, @recipes)
     @list_builder_hash = JSON.parse(@list.to_json)
   end
 
@@ -21,6 +23,12 @@ describe ListBuilder do
     expect(@list.format_user_information.keys).to eq(["user_id", "email"])
     expect(@list.format_user_information['email']).to eq("email@example.com")
     expect(@list.format_user_information['user_id']).to eq(@user.id)
+  end
+
+  it "correctly formats recipes information", :vcr do
+    expect(@list.format_recipes.class).to eq(Array)
+    expect(@list.format_recipes.count).to eq(2)
+    expect(@list.format_recipes.first.keys).to eq(["name", "source_url", "servings", "ingredients"])
   end
 
   it "correctly formats prepared to send information", :vcr do
