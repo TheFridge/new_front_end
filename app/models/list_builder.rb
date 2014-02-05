6c56d4c63cf2a5d9553d28a1c4907e2b33b968eb
Billy <navyosu@gmail.com>
2014-02-05 12:55:48 -0700

    Building some lists yo

--- app/models/list_builder.rb
+++ app/models/list_builder.rb
@@ -1,12 +1,19 @@
 class ListBuilder
 
+  attr_reader :user, :recipes
+
   def initialize(user, recipes)
     @user = user
     @recipes = recipes
   end
 
+  def format_user_information
+    {"user_id" => user.id, "email" => user.email}
+  end
+
   def to_send
-    @recipe.to_json
+    {"user" => format_user_information, "recipes" => recipes}.to_json
+    #recipes.to_json
   end
 
 end
--- spec/models/list_builder_spec.rb
+++ spec/models/list_builder_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe ListBuilder do
 
   before do
-    # @user = User.new(email: "email@example.com")
+    @user = User.new(email: "email@example.com")
     @recipe = Recipe.get_recipe
-    @list = ListBuilder.new(1, @recipe)
+    @list = ListBuilder.new(@user, @recipe)
     @list_builder_hash = JSON.parse(@list.to_json)
   end
 
@@ -17,7 +17,7 @@ describe ListBuilder do
     expect(@list_builder_hash.keys).to eq(["user", "recipes"])
   end
 
-  xit "has correct user hash keys", :vcr do
+  it "has correct user hash keys", :vcr do
     puts @list_builder_hash["recipes"].inspect
     expect(@list_builder_hash["user"].keys).to eq(["user_id", "email"])
   end
