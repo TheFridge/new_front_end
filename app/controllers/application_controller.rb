d54b854faa1ea78c46683f11fb621bd23a8fa933
Will Mitchell <wvmitchell@gmail.com>
2014-02-09 14:40:17 -0700

    if/else added to current user to handle dev/production needs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find_or_create_by(:email => cookies.signed['email']) if cookies.signed['email']
-    #  User.first
+    if Rails.env == 'development' || Rails.env == 'production'
+      User.first
+    else
+      User.find_or_create_by(:email => cookies.signed['email']) if cookies.signed['email']
+    end
   end
 
   def require_login
