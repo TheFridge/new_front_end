730e7c21818d637861e9e4afa94c9c777aedaa61
Rolen Le <rolentle@gmail.com>
2014-02-11 17:53:56 -0700

    created active tab feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def active_tab
+    actions = ['recipe','shopping_list', 'cupboard','favorites']
+    actions.each_with_object({}) do |action, hash|
+     hash[action] = "active" if current_page?(action: action)
+    end
+  end
 end
