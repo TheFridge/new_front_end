f63a5140748c395db22f31d71e1e6b445f7d9c4e
Will Mitchell <wvmitchell@gmail.com>
2014-02-13 14:17:07 -0700

    fade toggle on cupboard now functional

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  $('.cupboard-remove').click(function(e){
+    var table_row = $(this).parents('tr');
+    table_row.fadeToggle('slow');
+    removeIngredient(this);
+    return false;
+  });
+
+  function removeIngredient(element){
+    $.ajax({
+      type: 'DELETE',
+      url: $(element).attr('href')
+    });
+  }
+
+});
+
