de3a54c4645754f4bf1131a5562c67d5d4c5436f
Will Mitchell <wvmitchell@gmail.com>
2014-02-13 14:21:56 -0700

    fade toggle from shopping list now functional

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -7,6 +7,13 @@ $(function(){
     return false;
   });
 
+  $('.shopping-list-remove').click(function(e){
+    var table_row = $(this).parents('tr');
+    table_row.fadeToggle('slow');
+    removeIngredient(this);
+    return false;
+  });
+
   function removeIngredient(element){
     $.ajax({
       type: 'DELETE',
